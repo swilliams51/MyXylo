6a5d71b0629259c5571412f818a3fdb71da2cca3
steve williams <sjwilliams51@gmail.com>
2022-07-26 10:43:04 -0400

    Initial Commit

---
+++ MyXylophone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyXylophone
+//
+//  Created by Steven Williams on 7/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyXylophone/MyXylophoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyXylophoneApp.swift
+//  MyXylophone
+//
+//  Created by Steven Williams on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MyXylophoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
